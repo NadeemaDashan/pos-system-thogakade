93dc87a55db779ad8a1e0084a4432fbdcfc77c22
Dashan Nadeema <dashanwd@gmail.com>
2023-12-21 15:16:27 +0530

    item save done!

--- thogakade-pos/src/main/java/controller/ItemFormController.java
+++ thogakade-pos/src/main/java/controller/ItemFormController.java
@@ -1,9 +1,15 @@
 package controller;
 
+import dto.ItemDto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeTableColumn;
+import model.ItemModel;
+import model.impl.ItemModelImpl;
+
+import java.sql.SQLException;
 
 public class ItemFormController {
 
@@ -30,6 +36,7 @@ public class ItemFormController {
 
     @FXML
     private TextField txtUnitPrice;
+    private ItemModel itemModel = new ItemModelImpl();
 
     @FXML
     void btnDeleteActionPerformed(ActionEvent event) {
@@ -38,7 +45,24 @@ public class ItemFormController {
 
     @FXML
     void btnSaveActionPerformed(ActionEvent event) {
-
+        ItemDto itemDto = new ItemDto(
+                txtId.getText(),
+                txtDesc.getText(),
+                Double.parseDouble(txtUnitPrice.getText()),
+                Integer.parseInt(txtQty.getText())
+        );
+        try {
+            Boolean isSaved=itemModel.saveItem(itemDto);
+            if (isSaved){
+                new Alert(Alert.AlertType.CONFIRMATION,"Item Saved").show();
+            }else {
+                new Alert(Alert.AlertType.ERROR,"Something went wrong try again...").show();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
