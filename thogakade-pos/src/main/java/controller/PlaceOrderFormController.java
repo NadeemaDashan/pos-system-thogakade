c9622b8876558b37fd280fe4e36bf71e0567fde0
Dashan Nadeema <dashanwd@gmail.com>
2023-12-21 21:08:40 +0530

    Back button Action completed

--- thogakade-pos/src/main/java/controller/PlaceOrderFormController.java
+++ thogakade-pos/src/main/java/controller/PlaceOrderFormController.java
@@ -1,13 +1,18 @@
 package controller;
 
-import dto.tm.OrderTm;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PlaceOrderFormController {
 
@@ -18,25 +23,31 @@ public class PlaceOrderFormController {
     private ComboBox<?> cmbItemCode;
 
     @FXML
-    private TableColumn colAmount;
+    private TableColumn<?, ?> colAmount;
 
     @FXML
-    private TableColumn colDesc;
+    private TableColumn<?, ?> colDesc;
 
     @FXML
-    private TableColumn colId;
+    private TableColumn<?, ?> colId;
 
     @FXML
-    private TableColumn colOption;
+    private TableColumn<?, ?> colOption;
 
     @FXML
-    private TableColumn colQty;
+    private TableColumn<?, ?> colQty;
 
     @FXML
     private Label lblTotal;
 
     @FXML
-    private TableView<OrderTm> tblCart;
+    private AnchorPane pane;
+
+    @FXML
+    private Label lblOrderId;
+
+    @FXML
+    private TableView<?> tblCart;
 
     @FXML
     private TextField txtCusName;
@@ -53,6 +64,15 @@ public class PlaceOrderFormController {
     @FXML
     void btnAddToCartActionPerformed(ActionEvent event) {
 
+    }
+
+    @FXML
+    void btnBackActionPerformed(ActionEvent event) throws IOException {
+        Stage stage=(Stage)pane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/DashBoard.fxml"))));
+        stage.centerOnScreen();
+        stage.setTitle("DashBoard");
+        stage.show();
     }
 
     @FXML
