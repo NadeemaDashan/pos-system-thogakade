81aebcef7316d07ef8b3e2c97f3d9abe8ea534aa
Dashan Nadeema <dashanwd@gmail.com>
2023-12-21 22:58:10 +0530

    Generate ID and combo Box completion

--- thogakade-pos/src/main/java/controller/PlaceOrderFormController.java
+++ thogakade-pos/src/main/java/controller/PlaceOrderFormController.java
@@ -1,5 +1,11 @@
 package controller;
 
+import dto.CustomerDto;
+import dto.ItemDto;
+import dto.OrderDto;
+import dto.tm.OrderTm;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +17,16 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import model.CustomerModel;
+import model.ItemModel;
+import model.OrderModel;
+import model.impl.CustomerModelImpl;
+import model.impl.ItemModelImpl;
+import model.impl.OrderModelImpl;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 public class PlaceOrderFormController {
 
@@ -23,19 +37,19 @@ public class PlaceOrderFormController {
     private ComboBox<?> cmbItemCode;
 
     @FXML
-    private TableColumn<?, ?> colAmount;
+    private TableColumn colAmount;
 
     @FXML
-    private TableColumn<?, ?> colDesc;
+    private TableColumn colDesc;
 
     @FXML
-    private TableColumn<?, ?> colId;
+    private TableColumn colId;
 
     @FXML
-    private TableColumn<?, ?> colOption;
+    private TableColumn colOption;
 
     @FXML
-    private TableColumn<?, ?> colQty;
+    private TableColumn colQty;
 
     @FXML
     private Label lblTotal;
@@ -47,7 +61,7 @@ public class PlaceOrderFormController {
     private Label lblOrderId;
 
     @FXML
-    private TableView<?> tblCart;
+    private TableView<OrderTm> tblCart;
 
     @FXML
     private TextField txtCusName;
@@ -61,13 +75,19 @@ public class PlaceOrderFormController {
     @FXML
     private TextField txtUnitPrice;
 
+    private OrderModel orderModel = new OrderModelImpl();
+
+    private CustomerModel customerModel = new CustomerModelImpl();
+
+    private ItemModel itemModel = new ItemModelImpl();
+
     @FXML
     void btnAddToCartActionPerformed(ActionEvent event) {
 
     }
 
     @FXML
-    void btnBackActionPerformed(ActionEvent event) throws IOException {
+    public void btnBackActionPerformed(ActionEvent event) throws IOException {
         Stage stage=(Stage)pane.getScene().getWindow();
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/DashBoard.fxml"))));
         stage.centerOnScreen();
@@ -77,7 +97,50 @@ public class PlaceOrderFormController {
 
     @FXML
     void btnPlaceOrderActionPerformed(ActionEvent event) {
+    //TODO place Order is complex and should be done later......
+    }
+    public void initialize() throws SQLException, ClassNotFoundException {
+        generateId();
+        loadCustomerId();
+        loadItemCodes();
+    }
+    public void generateId() throws SQLException, ClassNotFoundException {
+        OrderDto orderDto =orderModel.lastOrder();
+        if (orderDto!=null){
+            String id=orderDto.getOrderId();
+            int num=Integer.parseInt(id.split("D")[1]);
+            num++;
+            lblOrderId.setText(String.format("D04%d",num));
+        }
+    }
 
+    private void loadCustomerId() {
+        try {
+            List<CustomerDto> customerList=customerModel.allCustomers();
+            ObservableList list = FXCollections.observableArrayList();
+            for (CustomerDto dto : customerList) {
+                list.add(dto.getId());
+            }
+            cmbCusId.setItems((ObservableList)list);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void loadItemCodes() {
+        try {
+            List<ItemDto> itemList=itemModel.allItems();
+            ObservableList list = FXCollections.observableArrayList();
+            for (ItemDto dto : itemList) {
+                list.add(dto.getCode());
+            }
+            cmbItemCode.setItems((ObservableList) list);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- thogakade-pos/src/main/java/model/impl/OrderModelImpl.java
+++ thogakade-pos/src/main/java/model/impl/OrderModelImpl.java
@@ -1,8 +1,11 @@
 package model.impl;
 
+import db.DBConnection;
 import dto.OrderDto;
 import model.OrderModel;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class OrderModelImpl implements OrderModel {
@@ -14,6 +17,17 @@ public class OrderModelImpl implements OrderModel {
 
     @Override
     public OrderDto lastOrder() throws SQLException, ClassNotFoundException {
+        String sql = "SELECT * FROM orders ORDER BY id DESC LIMIT 1";
+        PreparedStatement preparedStatement =DBConnection.getInstance().getConnection().prepareStatement(sql);
+        ResultSet resultSet =preparedStatement.executeQuery();
+        while (resultSet.next()){
+            return new OrderDto(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    null
+            );
+        }
         return null;
     }
 }
